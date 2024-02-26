34cea4438aa94342d691939c5b1e83e765b8bbd4
raja <raja.rakoto7@gmail.com>
2024-02-26 22:59:32 +0300

    modules: add all buttons2 snippets

--- modules/plum-buttons2.js
+++ modules/plum-buttons2.js
@@ -1,1 +1,57 @@
-export const plum_buttons2 = {};
+export const plum_buttons2 = {
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-3d',
+    body: ['@include buttons2-3d();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-borderless',
+    body: ['@include buttons2-borderless();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-border',
+    body: ['@include buttons2-border();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-dropdown',
+    body: ['@include buttons2-dropdown();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-glow',
+    body: ['@include buttons2-glow();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-groups',
+    body: ['@include buttons2-groups();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-longshadow',
+    body: ['@include buttons2-longshadow();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-raised',
+    body: ['@include buttons2-raised();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-shapes',
+    body: ['@include buttons2-shapes();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-sizes',
+    body: ['@include buttons2-sizes();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+  '[modules]: Buttons2 mixin from plum': {
+    prefix: 'plum-modules-buttons2-wrapper',
+    body: ['@include buttons2-wrapper();'],
+    description: '[modules]: Buttons2 mixin from plum',
+  },
+}
