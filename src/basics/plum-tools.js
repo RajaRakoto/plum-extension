7d3c3364bfd619fa2f8d426fda7bf97c841994e8
raja <raja.rakoto7@gmail.com>
2024-02-26 20:05:42 +0300

    src: remove

--- src/basics/plum-tools.js
+++
@@ -1,7 +0,0 @@
-export const plum_tools = {
-  '[tools]: Generate image from internet using link': {
-    prefix: 'plum-tools-randomPic',
-    body: ['https://picsum.photos/500/300?random=1'],
-    description: '[tools]: Generate image from internet using link',
-  },
-}
