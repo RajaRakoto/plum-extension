b4aa3589699411ea27209aa94bce7869050bceff
raja <raja.rakoto7@gmail.com>
2024-02-27 14:45:35 +0300

    refact | remove engine + reformat | apply BP

--- build.js
+++ build.js
@@ -1,42 +1,44 @@
-/* engine */
-import { concatJSONsourcesToOneFile } from './utilities/engine.js'
-/* basics snippets sources */
+/* libs */
+import fs from 'fs'
+/* src snippets sources */
 import { plum_animation } from './src/basics/plum-animation.js'
 import { plum_box } from './src/basics/plum-box.js'
-import { plum_color } from './src/basics/plum-color.js'
-import { plum_docs } from './src/basics/plum-docs.js'
 import { plum_effect } from './src/basics/plum-effect.js'
 import { plum_image } from './src/basics/plum-image.js'
-import { plum_import } from './src/basics/plum-import.js'
 import { plum_input } from './src/basics/plum-input.js'
-import { plum_layout } from './src/basics/plum-layout.js'
-import { plum_libraries } from './src/basics/plum-libraries.js'
-import { plum_media } from './src/basics/plum-media.js'
 import { plum_position } from './src/basics/plum-position.js'
 import { plum_spacing } from './src/basics/plum-spacing.js'
-import { plum_specs } from './src/basics/plum-specs.js'
-import { plum_tools } from './src/basics/plum-tools.js'
+import { plum_layout } from './src/layout/plum-layout.js'
+import { plum_media } from './src/media/plum-media.js'
+import { plum_specs } from './src/specs/plum-specs.js'
+import { plum_import } from './src/plum-import.js'
+/* modules snippets sources */
+import { plum_animate } from './modules/plum-animate.js'
+import { plum_buttons2 } from './modules/plum-buttons2.js'
+import { plum_hover2 } from './modules/plum-hover2.js'
+import { plum_magic } from './modules/plum-magic.js'
+import { plum_oc } from './modules/plum-oc.js'
 
 // ========================================
 
 // all snippets list
 const snippetsList = [
-  plum_animation,
-  plum_box,
-  plum_color,
-  plum_docs,
-  plum_effect,
-  plum_image,
-  plum_import,
-  plum_input,
-  plum_layout,
-  plum_libraries,
-  plum_media,
-  plum_position,
-  plum_spacing,
-  plum_specs,
-  plum_tools,
+  plum_animate,
+  plum_buttons2,
+  plum_hover2,
+  plum_magic,
+  plum_oc,
 ]
+// merge all snippets
+const concatenedSnippets = Object.assign(
+  {},
+  ...snippetsList.map((snippet) => {
+    return snippet
+  }),
+)
 
-// concat all snippets to one JSON file
-concatJSONsourcesToOneFile(snippetsList, './dist/plum.code-snippets')
+// convert to JSON
+const json = JSON.stringify(concatenedSnippets)
+
+// write to file
+fs.writeFileSync('./snippets.json', json)
