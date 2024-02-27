4c5b08afaf3ccdd52aa98b38e8224f8296e65080
raja <raja.rakoto7@gmail.com>
2024-02-27 11:26:55 +0300

    src: pre valid plum import snippets

--- src/plum-import.js
+++ src/plum-import.js
@@ -1,37 +1,67 @@
 export const plum_import = {
-  '[import]: Import basics plum module': {
-    prefix: 'plum-import-basics',
-    body: ["@import 'plum-basics';"],
-    description: '[import]: Import basics plum module',
+  '[import]: Import plum core': {
+    prefix: 'plum-import-plum',
+    body: ["@import '@rajarakoto/plum/plum';"],
+    description: '[import]: Import plum core',
   },
 
-  '[import]: Import milligrid (flexbox) module from plum': {
-    prefix: 'plum-import-milligrid',
-    body: ["@import 'plum-milligrid';"],
-    description: '[import]: Import milligrid (flexbox) module from plum',
-  },
-
-  '[import]: Import semantic-ui (min) module from plum': {
-    prefix: 'plum-import-semantic',
-    body: ["@import 'plum-semantic';"],
-    description: '[import]: Import semantic-ui (min) module from plum',
-  },
-
-  '[import]: Import bootstrap module (min) from plum': {
-    prefix: 'plum-import-bt',
-    body: ["@import 'plum-bootstrap';"],
-    description: '[import]: Import bootstrap module (min) from plum',
+  '[import]: Import animate module from plum': {
+    prefix: 'plum-import-animate',
+    body: ["@import '@rajarakoto/plum/modules/animate';"],
+    description: '[import]: Import animate module from plum',
   },
 
   '[import]: Import bootstrap (grid) module from plum': {
-    prefix: 'plum-import-bt-grid',
-    body: ["@import 'plum-bootstrap-grid';"],
+    prefix: 'plum-import-bootstrap-grid',
+    body: ["@import '@rajarakoto/plum/modules/bootstrap-grid';"],
     description: '[import]: Import bootstrap (grid) module from plum',
   },
 
-  '[import]: Import bootstrap module (utils) from plum': {
-    prefix: 'plum-import-bt-utils',
-    body: ["@import 'plum-bootstrap-utils';"],
-    description: '[import]: Import bootstrap module (utils) from plum',
+  '[import]: Import bootstrap (reboot) module from plum': {
+    prefix: 'plum-import-bootstrap-reboot',
+    body: ["@import '@rajarakoto/plum/modules/bootstrap-reboot';"],
+    description: '[import]: Import bootstrap (reboot) module from plum',
+  },
+
+  '[import]: Import bootstrap (utilities) module from plum': {
+    prefix: 'plum-import-bootstrap-utilities',
+    body: ["@import '@rajarakoto/plum/modules/bootstrap-utilities';"],
+    description: '[import]: Import bootstrap (utilities) module from plum',
+  },
+
+  '[import]: Import bootstrap module from plum': {
+    prefix: 'plum-import-bootstrap',
+    body: ["@import '@rajarakoto/plum/modules/bootstrap';"],
+    description: '[import]: Import bootstrap module from plum',
+  },
+
+  '[import]: Import buttons2 module from plum': {
+    prefix: 'plum-import-buttons2',
+    body: ["@import '@rajarakoto/plum/modules/buttons2';"],
+    description: '[import]: Import buttons2 module from plum',
+  },
+
+  '[import]: Import fontawesome free module from plum': {
+    prefix: 'plum-import-fontawesome-free',
+    body: ["@import '@rajarakoto/plum/modules/fontawesome-free';"],
+    description: '[import]: Import fontawesome free module from plum',
+  },
+
+  '[import]: Import hover2 module from plum': {
+    prefix: 'plum-import-hover2',
+    body: ["@import '@rajarakoto/plum/modules/hover2';"],
+    description: '[import]: Import hover2 module from plum',
+  },
+
+  '[import]: Import magic module from plum': {
+    prefix: 'plum-import-magic',
+    body: ["@import '@rajarakoto/plum/modules/magic';"],
+    description: '[import]: Import magic module from plum',
+  },
+
+  '[import]: Import open-color module from plum': {
+    prefix: 'plum-import-open-color',
+    body: ["@import '@rajarakoto/plum/modules/open-color';"],
+    description: '[import]: Import open-color module from plum',
   },
 }
