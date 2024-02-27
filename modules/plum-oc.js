2bc41731f7c12c4ca284b4d446d008a90da050df
raja <raja.rakoto7@gmail.com>
2024-02-27 16:14:32 +0300

    modules: hotfix

--- modules/plum-oc.js
+++ modules/plum-oc.js
@@ -1,79 +1,79 @@
 export const plum_oc = {
   '[modules]: OpenColor template from plum - gray': {
     prefix: 'plum-modules-oc-gray',
-    body: ['$oc-gray-${1:0~9};'],
+    body: ['$$oc-gray-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - gray',
   },
 
   '[modules]: OpenColor template from plum - red': {
     prefix: 'plum-modules-oc-red',
-    body: ['$oc-red-${1:0~9};'],
+    body: ['$$oc-red-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - red',
   },
 
   '[modules]: OpenColor template from plum - pink': {
     prefix: 'plum-modules-oc-pink',
-    body: ['$oc-pink-${1:0~9};'],
+    body: ['$$oc-pink-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - pink',
   },
 
   '[modules]: OpenColor template from plum - grape': {
     prefix: 'plum-modules-oc-grape',
-    body: ['$oc-grape-${1:0~9};'],
+    body: ['$$oc-grape-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - grape',
   },
 
   '[modules]: OpenColor template from plum - violet': {
     prefix: 'plum-modules-oc-violet',
-    body: ['$oc-violet-${1:0~9};'],
+    body: ['$$oc-violet-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - violet',
   },
 
   '[modules]: OpenColor template from plum - indigo': {
     prefix: 'plum-modules-oc-indigo',
-    body: ['$oc-indigo-${1:0~9};'],
+    body: ['$$oc-indigo-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - indigo',
   },
 
   '[modules]: OpenColor template from plum - blue': {
     prefix: 'plum-modules-oc-blue',
-    body: ['$oc-blue-${1:0~9};'],
+    body: ['$$oc-blue-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - blue',
   },
 
   '[modules]: OpenColor template from plum - cyan': {
     prefix: 'plum-modules-oc-cyan',
-    body: ['$oc-cyan-${1:0~9};'],
+    body: ['$$oc-cyan-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - cyan',
   },
 
   '[modules]: OpenColor template from plum - teal': {
     prefix: 'plum-modules-oc-teal',
-    body: ['$oc-teal-${1:0~9};'],
+    body: ['$$oc-teal-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - teal',
   },
 
   '[modules]: OpenColor template from plum - green': {
     prefix: 'plum-modules-oc-green',
-    body: ['$oc-green-${1:0~9};'],
+    body: ['$$oc-green-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - green',
   },
 
   '[modules]: OpenColor template from plum - lime': {
     prefix: 'plum-modules-oc-lime',
-    body: ['$oc-lime-${1:0~9};'],
+    body: ['$$oc-lime-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - lime',
   },
 
   '[modules]: OpenColor template from plum - yellow': {
     prefix: 'plum-modules-oc-yellow',
-    body: ['$oc-yellow-${1:0~9};'],
+    body: ['$$oc-yellow-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - yellow',
   },
 
   '[modules]: OpenColor template from plum - orange': {
     prefix: 'plum-modules-oc-orange',
-    body: ['$oc-orange-${1:0~9};'],
+    body: ['$$oc-orange-${1:0~9};'],
     description: '[modules]: OpenColor template from plum - orange',
   },
 }
