0ad56845a61ddf91a4b626d21e42b3898e4183a3
raja <raja.rakoto7@gmail.com>
2024-08-10 12:22:49 +0300

    exec biome

--- build.js
+++ build.js
@@ -65,4 +65,7 @@ const json = JSON.stringify(concatenedSnippets);
 // write to file
 const distDirectory = "./dist";
 ensureDirectoryExists(distDirectory);
-fs.writeFileSync(path.join(distDirectory, "plum-extension.code-snippets"), json);
+fs.writeFileSync(
+	path.join(distDirectory, "plum-extension.code-snippets"),
+	json,
+);
